f17aee32f5b7413b3455457e22a10bafbaf5975c
Jack Webster <jackson_webster@brown.edu>
2021-03-31 15:43:12 -0400

    Initial Setup - Main Class and ./run

---
+++ src/main/java/edu/brown/cs/abeckruiggallantjfraust2jwebste5/cs0320/Main.java
@@ -0,0 +1,117 @@
+package edu.brown.cs.abeckruiggallantjfraust2jwebste5.cs0320;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import spark.ExceptionHandler;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+import spark.TemplateViewRoute;
+import spark.template.freemarker.FreeMarkerEngine;
+import spark.Route;
+import org.json.JSONObject;
+import com.google.gson.Gson;
+
+import com.google.common.collect.ImmutableMap;
+import freemarker.template.Configuration;
+/**
+ * The Main class of our project. This is where execution begins.
+ */
+
+public final class Main {
+  private static final int DEFAULT_PORT = 4567;
+  private static final Gson GSON = new Gson();
+
+  /**
+   * The initial method called when execution begins.
+   *
+   * @param args An array of command line arguments
+   */
+  public static void main(String[] args) {
+    new Main(args).run();
+  }
+
+  private final String[] args;
+
+  private Main(String[] args) {
+    this.args = args;
+  }
+
+  /**
+   * This method runs the program, creating a new REPL
+   * to take in user input.
+   */
+  private void run() {
+    // Parse command line arguments
+    OptionParser parser = new OptionParser();
+    parser.accepts("gui");
+    parser.accepts("port").withRequiredArg().ofType(Integer.class)
+            .defaultsTo(DEFAULT_PORT);
+    OptionSet options = parser.parse(args);
+
+    if (options.has("gui")) {
+      runSparkServer((int) options.valueOf("port"));
+    }
+    System.out.println("Running");
+  }
+
+  private static FreeMarkerEngine createEngine() {
+    Configuration config = new Configuration();
+    File templates = new File("src/main/resources/spark/template/freemarker");
+    try {
+      config.setDirectoryForTemplateLoading(templates);
+    } catch (IOException ioe) {
+      System.out.printf("Message: Unable use %s for template loading.%n",
+              templates);
+      System.exit(1);
+    }
+    return new FreeMarkerEngine(config);
+  }
+
+  private void runSparkServer(int port) {
+    Spark.port(port);
+    Spark.externalStaticFileLocation("src/main/resources/static");
+    Spark.exception(Exception.class, new ExceptionPrinter());
+    FreeMarkerEngine freeMarker = createEngine();
+    // Setup Spark Route
+    Spark.options("/*", (request, response) -> {
+      String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+      if (accessControlRequestHeaders != null) {
+        response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+      }
+      String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+      if (accessControlRequestMethod != null) {
+        response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+      }
+      return "OK";
+    });
+    Spark.before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+    Spark.exception(Exception.class, new ExceptionPrinter());
+  }
+
+  /**
+   * Display an Message page when an exception occurs in the server.
+   */
+  private static class ExceptionPrinter implements ExceptionHandler {
+    @Override
+    public void handle(Exception e, Request req, Response res) {
+      res.status(500);
+      StringWriter stacktrace = new StringWriter();
+      try (PrintWriter pw = new PrintWriter(stacktrace)) {
+        pw.println("<pre>");
+        e.printStackTrace(pw);
+        pw.println("</pre>");
+      }
+      res.body(stacktrace.toString());
+    }
+  }
+}
