78b82b8da62cb2395bfe9590f0703a87ef5a5ab2
jfrausto7 <jacob_frausto@brown.edu>
2021-04-05 13:34:27 -0400

    setup recipe page

--- src/main/recipe-finder/src/Recipe.js
+++ src/main/recipe-finder/src/Recipe.js
@@ -2,6 +2,7 @@ import List from "./List";
 import React, {useState, useEffect, useRef} from 'react';
 import SubmitButton from "./SubmitButton";
 import Button from 'react-bootstrap/Button';
+import SimilarRecipe from "./SimilarRecipe";
 
 function Recipe() {
 
@@ -22,13 +23,19 @@ function Recipe() {
 
 
     return (
-        <div style={rootStyle} className="Fridge">
+        <div style={rootStyle} className="Recipe">
             {/*dynamic header*/}
             <h1 style={{top: 25, color: "#000"}}>{name}</h1>
             {/*two buttons on side of page*/}
             <Button variant="success" size= "lg" style={{position: "absolute", left: 50, top: 25}}>Back to Fridge</Button>
             <Button variant="primary" size= "lg" style={{position: "absolute", right: 50, top: 25}}>Cook!</Button>
 
+            {/*three similar recipes as suggestions*/}
+            <div className={"flex-container"}>
+                <SimilarRecipe label={"Recipe 1"}/>
+                <SimilarRecipe label={"Recipe 2"}/>
+                <SimilarRecipe label={"Recipe 3"}/>
+            </div>
         </div>
     );
 }
